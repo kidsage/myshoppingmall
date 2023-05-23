490fc266e40403a23a872356ad3031555e56b83f
kidsage <ruhyun92@gmail.com>
2023-05-23 21:23:41 +0900

    Edit user, userprofile
    - user에서 필요없는 라인 삭제
    - profile required, optional 분리 후 항목 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,40 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
+    def create_user(self, email, password=None):
+        """
+        주어진 이메일, 비밀번호 등 개인정보로 User 인스턴스 생성
+        """
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
             email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, date_of_birth, password):
+    def create_superuser(self, email, password):
         user = self.create_user(
             email,
             password=password,
-            date_of_birth=date_of_birth,
         )
         user.is_admin = True
         user.save(using=self._db)
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name='email',
         max_length=255,
         unique=True,
     )
+    date_joined = models.DateTimeField(
+        default=timezone.now
+    )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+
 
     objects = UserManager()
 
@@ -54,7 +65,7 @@ class User(AbstractBaseUser):
         return self.is_admin
 
 
-class Profile(models.Model):
+class UserProfile(models.Model):
 
     GENDER = (
         ("M", "Male"),
@@ -62,13 +73,27 @@ class Profile(models.Model):
         ("T", "TransSexual")
     )
 
+    # Required
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=10)
     nickname = models.CharField(max_length=20)
-    bio = models.TextField()
-    date_of_birth = models.DateField()
-    gender = models.CharField()
+    email = models.EmailField()
+    phonenumber = PhoneNumberField()
 
-    REQUIRED_FIELDS = ['date_of_birth']
+    # Optional
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=2, choices=GENDER, blank=True, null=True)
+    tall = models.SmallIntegerField(blank=True, null=True)
+    weight = models.SmallIntegerField(blank=True, null=True)
+    address = models.CharField(max_length=100)
+    
+    REQUIRED_FIELDS = ['nickname', 'date_of_birth', 'gender']
 
     def __str__(self):
+        return self.nickname
+    
+    def get_full_name(self):        
+        return self.nickname
+
+    def get_short_name(self):
         return self.nickname
