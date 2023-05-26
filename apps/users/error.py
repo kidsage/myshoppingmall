5619511a766d522d67c52a191bf05a15c3a7ef3e
kidsage <ruhyun92@gmail.com>
2023-05-26 13:57:41 +0900

    Edit something

--- apps/users/error.py
+++ apps/users/error.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
-class UsernameAlreadyExistsError(APIException):
+class EmailAlreadyExistsError(APIException):
     status_code = status.HTTP_409_CONFLICT
-    default_detail = "이미 존재하는 username입니다."
-    default_code = "username_already_exists_error"
+    default_detail = "이미 존재하는 email입니다."
+    default_code = "email_already_exists_error"
