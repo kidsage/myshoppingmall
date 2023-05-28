8ff100d564adf00e66b2cf646a23db014927e398
kidsage <ruhyun92@gmail.com>
2023-05-28 13:33:06 +0900

    + Initialize router set

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from types import ModuleType
+from typing import Optional
 
-# Create your views here.
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+
+def viewset_path(
+    viewset, prefix: str, basename: Optional[str] = None
+) -> tuple[ModuleType, Optional[bool], Optional[bool]]:
+    router = DefaultRouter()
+    router.register(prefix, viewset, basename=basename or prefix)
+    return path(f'{prefix}', include(router.urls))
