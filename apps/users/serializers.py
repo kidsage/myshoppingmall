035b0df578870b01625669c91a34772efb83f76a
kidsage <ruhyun92@gmail.com>
2023-05-30 15:46:01 +0900

    Feat: + set user serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,12 +1,35 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fileds = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(source='userprofile', many=False)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email', 'date_joined', 'is_active', 'profile')
+
+    # Custom .update() method for serializer to handle UserProfile data update
+    def update(self, instance, validated_data):
+        userprofile_serializer = self.fields['profile']
+        userprofile_instance = instance.userprofile
+        userprofile_data = validated_data.pop('userprofile', {})
+
+        # to access the UserProfile fields in here
+        # mobile = userprofile_data.get('mobile')
+
+        # update the userprofile fields
+        userprofile_serializer.update(userprofile_instance, userprofile_data)
+
+        instance = super().update(instance, validated_data)
+        return instance
 
 
 class LoginSerializer(serializers.Serializer):
