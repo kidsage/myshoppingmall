ff8acd5dad02983f89a41ab7ef7329fde9d69f6e
kidsage <ruhyun92@gmail.com>
2023-06-02 13:44:31 +0900

    + edit base settings

--- myshoppingmall/settings/base.py
+++ myshoppingmall/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My apps
     'apps.users',
+    'apps.product',
     # Oterh apps
     'phonenumber_field',
 ]
@@ -82,13 +83,12 @@ WSGI_APPLICATION = 'myshoppingmall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
