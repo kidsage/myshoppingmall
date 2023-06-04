4edd816e561685f7077efd98be063a304c6b0bec
kidsage <ruhyun92@gmail.com>
2023-06-04 22:40:21 +0900

    initialize product model

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import BaseModel
 
 # Create your models here.
+class Product(BaseModel):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
