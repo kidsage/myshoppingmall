6d3556706f53f229b772bc14591fb21df18f4653
kidsage <ruhyun92@gmail.com>
2023-06-07 22:32:50 +0900

    + initialize product view

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
