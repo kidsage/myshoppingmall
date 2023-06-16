4b52366cca9360e02de82b5c74d1467aed32d045
kidsage <ruhyun92@gmail.com>
2023-06-17 02:32:18 +0900

    - Fix: migrate error

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -6,7 +6,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
