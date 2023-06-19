b4f20352af27b89401436e4f40750ba8280a1713
kidsage <ruhyun92@gmail.com>
2023-06-19 18:19:29 +0900

    - Fix: change .py name

--- myshoppingmall/urls.py
+++ myshoppingmall/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('apps.users.urls')),
-    path('api/product/', include('apps.product.urls')),
+    path('', include('apps.users.urls')),
+    path('', include('apps.product.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
