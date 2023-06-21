e7f1a76a9c1abbc07ebb284f3d22ebf6c4a12cf8
kidsage <ruhyun92@gmail.com>
2023-06-21 18:58:41 +0900

    + product url

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from core.urls import viewset_path
+from apps.product.views import ProductViewSet
+
+
 urlpatterns = [
-    
+    viewset_path(ProductViewSet, "product"),
 ]
