7853a5c1dad6495e723e9ed1863aae9758db6d0a
kidsage <ruhyun92@gmail.com>
2023-06-22 16:58:24 +0900

    + profile url test

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from core.urls import viewset_path
-from apps.users.views import UserViewSet
+from apps.users.views import UserViewSet, UserProfileViewSet
 
 
 urlpatterns = [
     viewset_path(UserViewSet, "users"),
+    viewset_path(UserProfileViewSet, "profile"),
 ]
