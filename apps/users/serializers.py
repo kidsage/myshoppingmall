ca13f041a266090d0d93ed0fdc9f84d877984dd6
kidsage <ruhyun92@gmail.com>
2023-06-29 17:52:47 +0900

    separate user, profile

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -10,26 +10,26 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile = UserProfileSerializer(source='userprofile', many=False)
+    # profile = UserProfileSerializer(source='userprofile', many=False)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'profile')
+        fields = ('email', 'password') # , 'profile')
     
     # Custom .update() method for serializer to handle UserProfile data update
-    def update(self, instance, validated_data):
-        userprofile_serializer = self.fields['profile']
-        userprofile_instance = instance.userprofile
-        userprofile_data = validated_data.pop('userprofile', {})
+    # def update(self, instance, validated_data):
+    #     userprofile_serializer = self.fields['profile']
+    #     userprofile_instance = instance.userprofile
+    #     userprofile_data = validated_data.pop('userprofile', {})
 
-        # to access the UserProfile fields in here
-        # mobile = userprofile_data.get('mobile')
+    #     # to access the UserProfile fields in here
+    #     # mobile = userprofile_data.get('mobile')
 
-        # update the userprofile fields
-        userprofile_serializer.update(userprofile_instance, userprofile_data)
+    #     # update the userprofile fields
+    #     userprofile_serializer.update(userprofile_instance, userprofile_data)
 
-        instance = super().update(instance, validated_data)
-        return instance
+    #     instance = super().update(instance, validated_data)
+    #     return instance
 
 
 class LoginSerializer(serializers.Serializer):
