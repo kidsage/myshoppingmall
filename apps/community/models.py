039bf7129da86a457c77a04f1fc3ca88c8269c32
kidsage <ruhyun92@gmail.com>
2023-06-30 13:21:21 +0900

    + initialize commu model

--- apps/community/models.py
+++ apps/community/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from core.models import BaseModel
+from apps.users.models import User
+
 # Create your models here.
+class Post(BaseModel):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True)
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='klass')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    contents = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='reply')
